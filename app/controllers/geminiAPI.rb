c66e5b1c7ec910d375b65f568a94d8ba1a5e67e9
clemberard <clement.berard@ecole-isitech.fr>
2024-09-16 11:48:54 +0200

    first commit

---
+++ app/controllers/geminiAPI.rb
@@ -0,0 +1,5 @@
+class GeminiAPIController < ActionController::Base
+  def index
+    render plain: "Hello, World!"
+  end
+end
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    @test = 'test'
+  end
+end
